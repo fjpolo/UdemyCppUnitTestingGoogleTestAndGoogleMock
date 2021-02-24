40cc9c1fe24483105f8fdc3f359a3429c795afaa
franco.polo <franco.polo@ii-vi.com>
2021-02-24 15:26:41 +0100

    Section 3.12 done.-

---
+++ 003GoogleTestAAA/build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/fpolo/Downloads/Franco/Courses/Coursera/GoogleTest/003GoogleTestAAA/googletest/googletest
+# Build directory: C:/Users/fpolo/Downloads/Franco/Courses/Coursera/GoogleTest/003GoogleTestAAA/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
