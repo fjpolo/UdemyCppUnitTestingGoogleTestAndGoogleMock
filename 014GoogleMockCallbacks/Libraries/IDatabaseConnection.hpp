b340d3200653101b4bc26adcead239c23b249a5e
franco.polo <franco.polo@ii-vi.com>
2021-03-02 12:06:30 +0100

    Section 6.47 done.-

---
+++ 014GoogleMockCallbacks/Libraries/IDatabaseConnection.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <functional>
+
+#include "Employee.hpp"
+
+//interface because we might subclass it
+//example: IOracleDatabaseConnectionManager, ISQLDatabaseConnectionManager
+class IDatabaseConnection{
+  public:
+    using Callback = std::function<void(int, int)>;
+    /*Constructor*/
+    IDatabaseConnection(std::string serverAddress);
+    ~IDatabaseConnection();
+    /*Facilitators*/
+    virtual float getSalary(int id) const = 0; // SELECT * FROM employees WHERE id=...
+    virtual void updateSalary(int id, float newSalary) = 0;
+    virtual std::vector<Employee> getSalariesRange(float low) const = 0; //just greater than low
+    virtual std::vector<Employee> getSalariesRange(float low, float high) const = 0;
+    /*Other*/
+    virtual void connect();
+    virtual void disconnect();
+    void setOnConnect(Callback onConnect);
+
+  protected:
+    /*Attributes*/
+    std::string mServerAddress;
+    Callback mOnConnect;
+};
