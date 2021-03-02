afea06cd7d0737541d5e79bd037165d014209180
franco.polo <franco.polo@ii-vi.com>
2021-03-02 11:06:15 +0100

    Section 6.45 done

---
+++ 012GoogleMockInvoke_backup/build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/fpolo/Downloads/Franco/Courses/Coursera/GoogleTest/012GoogleMockInvoke_backup/googletest/googletest
+# Build directory: C:/Users/fpolo/Downloads/Franco/Courses/Coursera/GoogleTest/012GoogleMockInvoke_backup/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
