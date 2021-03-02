8d9ae9cd13768df6f33a0feb1d3abc8d64aaa081
franco.polo <franco.polo@ii-vi.com>
2021-03-02 10:13:34 +0100

    Section 6.42 done.-

--- 011GoogleTestProject_backup_mock_full/tests/testRunner.cpp
+++ 011GoogleTestProject_backup_mock_full/tests/testRunner.cpp
@@ -42,6 +42,40 @@ TEST(TestEmployeeManager, TestConnection){
 }
 
 /**
+ * TestEmployeeManager - TestUpdateSalary
+ */
+TEST(TestEmployeeManager, TestUpdateSalary)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+    EXPECT_CALL(dbConnection, connect());
+    EXPECT_CALL(dbConnection, disconnect());
+    EXPECT_CALL(dbConnection, updateSalary(testing::_, testing::_)).Times(1);   
+
+    EmployeeManager employeeManager(&dbConnection);
+
+    employeeManager.setSalary(50, 6000);
+}
+
+/**
+ * TestEmployeeManager - TestGetSalary
+ */
+TEST(TestEmployeeManager, TestGetSalary)
+{
+    const int employeeId = 50;
+    const float salary = 6100.0;
+    MockDatabaseConnection dbConnection("dummyConnection");
+    EXPECT_CALL(dbConnection, connect());
+    EXPECT_CALL(dbConnection, disconnect());
+    EXPECT_CALL(dbConnection, getSalary(testing::_)).Times(1).WillOnce(testing::Return(salary));
+
+    EmployeeManager employeeManager(&dbConnection);
+
+    float returnedSalary = employeeManager.getSalary(employeeId);
+
+    ASSERT_EQ(salary, returnedSalary);
+}
+
+/**
  * main runner
  */
 int main(int argc, char **argv)
